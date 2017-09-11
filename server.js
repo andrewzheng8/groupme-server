f5965f7fea40c7d96138ae4318c0ed0a696966e8
andrewzheng8 <andrew.zheng725@gmail.com>
2017-09-11 17:17:26 -0400

    set up of server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan') // logger
 const app = express()
 const router = require('./router')
-//add const mongoose = require('mongoose')
+// add const mongoose = require('mongoose')
 const cors = require('cors')
 
 // DB Setup
@@ -18,9 +18,16 @@ app.use(bodyParser.json({ type: '*/*' }))
 router(app)
 // app.use middleware
 // Server Setup
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 const server = http.createServer(app)
-server.listen(port)
-console.log('Server listening on:', port)
+const io = require('socket.io')(server)
+
+io.on('connection', function (socket) {
+  console.log('a user connected')
+})
+
+server.listen(port, function () {
+  console.log('Server listening on:', port)
+})
 
 // add protocol and https for adding to heroku
